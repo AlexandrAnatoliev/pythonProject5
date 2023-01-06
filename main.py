98272b2d52d83f333495c82016182c894ad32dae
AlexandrAnatoliev <per-1986@list.ru>
2023-01-06 20:52:37 +0300

    Из текста 1 удаляет короткие и неполные рецепты

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def open_text1(text):
     """
     file1 = open(text, encoding='utf-8')  # если файл находится не в проекте, то писать полный путь:
     # "C:/Users/Александр/OneDrive/Рабочий стол/python/FreelanceTask2/freelanceTask3/firstText.txt" (использ.:'/'!)
-    file1_read = list(file1.read().split(sep='\n\n'))  # сохраняет данные из файла в список в str формате
+    file1_read = list(file1.read().split(sep='\n\n\n'))  # сохраняет данные из файла в список в str формате
     file1.close()  # закрывает файл
     return file1_read
 
@@ -35,9 +35,9 @@ def clean_text(text):
     cl_text = cl_text.replace(',', ', ')
     while ',  ' in cl_text:
         cl_text = cl_text.replace(',  ', ', ')
-    cl_text=cl_text.replace('.','. ')
+    cl_text = cl_text.replace('.', '. ')
     while '.  ' in cl_text:
-        cl_text=cl_text.replace('.  ','. ')
+        cl_text = cl_text.replace('.  ', '. ')
     cl_text = cl_text.replace('!', '! ')
     while '!  ' in cl_text:
         cl_text = cl_text.replace('!  ', '! ')
@@ -52,13 +52,13 @@ def clean_text(text):
         cl_text = cl_text.replace('...  ', '... ')
     # добавляем переносы
     while ":-" in cl_text:
-        cl_text= cl_text.replace(":-",":\n-")
+        cl_text = cl_text.replace(":-", ":\n-")
     while ": -" in cl_text:
-        cl_text= cl_text.replace(": -",":\n-")
+        cl_text = cl_text.replace(": -", ":\n-")
     while ".-" in cl_text:
-        cl_text= cl_text.replace(".-",".\n-")
+        cl_text = cl_text.replace(".-", ".\n-")
     while ". -" in cl_text:
-        cl_text= cl_text.replace(". -",".\n-")
+        cl_text = cl_text.replace(". -", ".\n-")
     while "?-" in cl_text:
         cl_text = cl_text.replace("?-", "?\n-")
     while "? -" in cl_text:
@@ -90,6 +90,21 @@ def clean_text(text):
     return cl_text
 
 
+def delete_chort_text(text1):
+    """
+    Из текста 1 удаляет короткие и неполные рецепты
+    :param text1: Список с рецептами
+    :return: Список с рецептами
+    """
+    if len(text1[:text1.index("PEЦEПT:")]) < 5:
+        text2 = None
+    elif len(text1[text1.index("PEЦEПT:"):]) < 5:
+        text2 = None
+    else:
+        text2 = text1
+    return text2
+
+
 fun_list = open_text1(open_txt)
 print(fun_list)
 
@@ -97,18 +112,21 @@ file2 = open("secondText.txt", 'w', encoding='utf-8')  # создается фа
 
 # Очищеная страница записывается в список 'jokes' и в текстовый файл 'secondText.txt'
 jokes = []
-for joke in fun_list:
-    fl_stop = False
-    for stop in stop_list:
-        fl_stop = False
-        if stop in joke:
-            fl_stop = True
-        if fl_stop is True:
-            break
-    if fl_stop is False and joke != '':  # joke != '' - убираем пустые строки
-        jokes.append(clean_text(joke))
-        file2.write(clean_text(joke) + '\n\n')
+# for joke in fun_list:
+#    fl_stop = False
+#   for stop in stop_list:
+#       fl_stop = False
+#       if stop in joke:
+#           fl_stop = True
+#       if fl_stop is True:
+#           break
+#   if fl_stop is False and joke != '':  # joke != '' - убираем пустые строки
+#       jokes.append(clean_text(joke))
+#       file2.write(clean_text(joke) + '\n\n')
 
+for recipe in fun_list:
+    if delete_chort_text(recipe) != None:
+        jokes.append(recipe)
+        file2.write(recipe + '\n\n')
 file2.close()  # закрывает файл
-# странные проблемы
 print(jokes)
